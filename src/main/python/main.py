3f77f9eddb19fdbf43368b479531a0206a9d5a58
oduvan <a.lyabah@checkio.org>
2020-09-27 18:26:34 +0300

    systray

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,32 +1,54 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext, cached_property
-from PyQt5.QtWidgets import QMainWindow, QLabel
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QLabel, QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtGui import QImage, QPixmap, QIcon
 from PyQt5.QtCore import  Qt
 
 import sys
-
-class MainWindow(QMainWindow):
-    def __init__(self, ctx):
-        super().__init__()
-        self.ctx = ctx
-
-        l = QLabel(self)
-        l.setPixmap(QPixmap.fromImage(self.ctx.img_smile))
-        l.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-        # self.addWidget(l)
+import webbrowser
 
 class AppContext(ApplicationContext):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.window = MainWindow(self)
+        #self.window = MainWindow(self)
+        #self.stray = SystemTray(self)
 
     def run(self):
-        self.window.show()
+        app = self.app
+
+        app.setQuitOnLastWindowClosed(False)
+
+        # Create the icon
+        icon = QIcon(self.img_icon)
+
+        # Create the tray
+        tray = QSystemTrayIcon()
+        tray.setIcon(icon)
+        tray.setVisible(True)
+
+        # Create the menu
+        menu = QMenu()
+        open_site = QAction("Open")
+        open_site.triggered.connect(self.open_site)
+
+        menu.addAction(open_site)
+
+        # Add a Quit option to the menu.
+        quit = QAction("Quit")
+        quit.triggered.connect(app.quit)
+        menu.addAction(quit)
+
+        # Add the menu to the tray
+        tray.setContextMenu(menu)
+
         return self.app.exec_()
 
+    def open_site(self):
+        webbrowser.open_new_tab('https://checkio.org/')
+
+
     @cached_property
-    def img_smile(self):
-        return QImage(self.get_resource('smile.png'))
+    def img_icon(self):
+        return QIcon(self.get_resource('icon.png'))
 
 if __name__ == '__main__':
     appctxt = AppContext()      # 2. Invoke appctxt.app.exec_()
